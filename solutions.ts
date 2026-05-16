533e3ad34e3b0dac66e11e51f05a54dd93e7ffea
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-05-17 02:51:17 +0600

    done problem set

--- solutions.ts
+++ solutions.ts
@@ -1,8 +1,7 @@
 // Problem 1
 const filterEvenNumbers = (arr: number[]):number[] => {
-    return arr.filter(arr => arr % 2 == 0);
+    return arr.filter(arr => arr % 2 === 0);
 }
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
 
 
@@ -10,7 +9,6 @@ console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 const reverseString = (str: string):string =>{
     return str.split('').reverse().join("")
 }
-console.log(reverseString("typescript"))
 
 
 
@@ -25,13 +23,21 @@ const checkType = (params: StringOrNumber): string =>{
     return "String"
 }
 
-console.log(checkType("rafi"))
-console.log(checkType(29))
 
 
 
 
 // problem 4
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] =>{
+    return obj[key];
+}
+const user = { id: 1, name: "John Doe", age: 21 }
+
+
+
+
+
+// problem 5
 interface Book {
     title: string;
     author: string;
@@ -50,9 +56,35 @@ const toggleReadStatus = (params: Book): BookWithReadStatus => {
 }
 
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-console.log(toggleReadStatus(myBook))
 
 
 
-// problem 5
+// problem 6
+class Person {
+    public name: string;
+    public age: number;
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}
 
+class Student extends Person{
+    public grade: string;
+    constructor(name: string, age: number, grade: string){
+        super(name, age);
+        this.grade = grade;
+    }
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+    }
+}
+
+const student = new Student("Alice", 20, "A");
+
+
+
+// problem 7
+const getIntersection = (arr1: number[], arr2: number[]): number[] => {
+    return arr1.filter(element=> arr2.includes(element))
+}
